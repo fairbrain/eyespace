265948450ed9aed6a2805335a69af73f6cf3e9da
fairbrain <fairbrainpacksol@gmail.com>
2024-09-15 15:15:29 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -86,9 +86,10 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+    navbar.classList.add("fixed-top");
+    navbar.classList.add("bg-white");
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("fixed-top");
   }
 }
 
